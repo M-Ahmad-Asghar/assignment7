a876667455ac43383d6f35b57d4f18bd94299307
M.Ahmad.Asghar <ahmadasg2366@gmail.com>
2020-12-27 20:50:54 +0500

    programme4

---
+++ script.js
@@ -0,0 +1,17 @@
+var input = parseInt(prompt("Enter a Number 2 or 3)"));
+var process = 2;
+var process2 = 3;
+
+
+var message = "Even" ;
+var message2 = "odd" ;
+ 
+if(input === process){
+    alert( message); 
+}
+if(input === process2){
+    alert(message2)
+}
+
+
+
